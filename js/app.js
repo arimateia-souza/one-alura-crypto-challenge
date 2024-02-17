3dfe347c5f8dd00333b1bad5797874f28de382d0
Ari Souza <email.arisantos@gmail.com>
2024-02-16 23:55:04 -0300

    unicode ao texto

--- js/app.js
+++ js/app.js
@@ -2,25 +2,34 @@ let mensagem = document.querySelector('textarea'); //pega texto do input
 let img = document.getElementById('imagem_ilustrativa');
 let mensagemCriptografada = '';
 let botaoCopiar = document.getElementById('botao__copiar');
+let notificacaoCopia = document.getElementById('notificacao-copia');
 
+
+// Criptografar
 function criptografar() {
     if (mensagem.value == '') { 
         verificaTexto();
     } else {
-        mensagemCriptografada = mensagem.value
+        //unicode tira acentos
+        var novaMensagem = mensagem.value.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        novaMensagem = novaMensagem.toLowerCase();
+        console.log(novaMensagem);
+        mensagemCriptografada = novaMensagem
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
+        //escreve no html
         document.getElementById('botao__copiar').removeAttribute('hidden');
         img.style.display = 'none';
         alteraTexto('h2', '');
         alteraTexto('p', mensagemCriptografada);//valor do input
         mensagem.value = '';//limpa campo
+
     }  
 }
-
+// Descriptografar
 function descriptografar() {
     if (mensagem.value == '') { 
         verificaTexto();
@@ -47,17 +56,31 @@ function verificaTexto() {
         alteraTexto('h2', 'Mensagem inválida!');
         alteraTexto('p', 'Digite uma mensagem válida para criptografar ou descriptografar');//valor do input
         mensagem.value = '';//limpa campo
-
 }
 
 function alteraTexto(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-botaoCopiar.addEventListener("click", (evt) => {
-    navigator.clipboard.writeText(mensagemCriptografada);
 
+botaoCopiar.addEventListener("click", () => {
+    navigator.clipboard.writeText(mensagemCriptografada)
+        .then(() => {
+            notificacaoCopia.style.display = 'block';
+            setTimeout(() => {
+                notificacaoCopia.style.display = 'none';
+            }, 6000);
+        })
+        .catch((err) => {
+            console.error('Erro ao copiar o texto:', err);
+        });
 });
+/*Validação 17
+Como requisito do projeto, não serão aceitas
+letras maiúsculas e caracteres especiais.
+Pesquise sobre a tabela UNICODE.*/
+
+
 
 
 
